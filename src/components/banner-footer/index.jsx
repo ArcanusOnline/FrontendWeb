64370fe67b9217be45e0235a7d92a239c94f64a0
Gonzalo Bazan <aygron@gmail.com>
2025-05-12 18:29:55 -0300

    Update index.jsx
    Agrego redes sociales

--- src/components/banner-footer/index.jsx
+++ src/components/banner-footer/index.jsx
@@ -6,38 +6,38 @@ const BannerFooter = () => {
       <div className="bannerFooter">
         <ul>
           <li>
-            <a href="https://www.facebook.com" target="_blank">
+            <a href="https://www.facebook.com/ArcanusOnline/" target="_blank">
               <img src={urlImagenes.facebook} alt="Logo Facebook" />
-              Facebook
             </a>
           </li>
           <li>
-            <a href="https://www.discord.com" target="_blank">
+            <a href="https://discord.com/invite/Eqg7zmVj2f" target="_blank">
               <img src={urlImagenes.discord} alt="Logo Discord" />
-              Discord
             </a>
           </li>
           <li>
-            <a href="https://www.youtube.com" target="_blank">
+            <a href="https://www.youtube.com/@ArcanusOnline" target="_blank">
               <img src={urlImagenes.youtube} alt="Logo Youtube" />
-              Youtube
             </a>
           </li>
           <li>
-            <a href="https://www.threads.com" target="_blank">
+            <a href="https://www.threads.com/@arcanusonline" target="_blank">
               <img src={urlImagenes.threads} alt="Logo Threads" />
-              Threads
             </a>
           </li>
           <li>
-            <a href="https://www.x.com" target="_blank">
-              <img src={urlImagenes.twitter} alt="Logo Twitter" />X
+            <a href="https://x.com/ArcanusOnline_" target="_blank">
+              <img src={urlImagenes.twitter} alt="Logo Twitter" />
             </a>
           </li>
           <li>
-            <a href="https://www.instagram.com" target="_blank">
+            <a href="https://instagram.com/arcanusonline" target="_blank">
               <img src={urlImagenes.instagram} alt="Logo Instagram" />
-              Instagram
+            </a>
+          </li>
+          <li>
+            <a href="https://www.twitch.tv/arcanusonline" target="_blank">
+              <img src={urlImagenes.twitch} alt="Logo Twitch" />
             </a>
           </li>
         </ul>
