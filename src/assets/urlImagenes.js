b08a4fadcf353caf8792589755e8d4df76c4c804
Santiago <san_chan97@hotmail.com>
2025-08-05 16:27:25 -0300

    add discord white logo en el header

--- src/assets/urlImagenes.js
+++ src/assets/urlImagenes.js
@@ -1,22 +1,23 @@
 import logo from "../resources/images/logo.png";
-import facebook from "../resources/images/facebook.png"
-import twitter from "../resources/images/twitter.png"
-import discord from "../resources/images/discord.png"
-import threads from "../resources/images/threads.png"
-import youtube from "../resources/images/youtube.png"
-import instagram from "../resources/images/instagram.png"
-import twitch from "../resources/images/twitch.png"
-
+import facebook from "../resources/images/facebook.png";
+import twitter from "../resources/images/twitter.png";
+import discord from "../resources/images/discord.png";
+import discordWhite from "../resources/images/discordWhite.png";
+import threads from "../resources/images/threads.png";
+import youtube from "../resources/images/youtube.png";
+import instagram from "../resources/images/instagram.png";
+import twitch from "../resources/images/twitch.png";
 
 let urlImagenes = {
   logo,
   facebook,
   twitter,
-  discord,  
+  discord,
+  discordWhite,
   threads,
   youtube,
   instagram,
-  twitch
+  twitch,
 };
 
 export { urlImagenes };
