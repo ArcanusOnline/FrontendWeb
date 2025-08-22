64d22554e73a5817a69e30a404344be7334f96c6
Santiago <san_chan97@hotmail.com>
2025-08-22 14:40:38 -0300

    Update reglas indice json & reglas max-width

--- src/pages/reglas/index.jsx
+++ src/pages/reglas/index.jsx
@@ -1,17 +1,15 @@
-import {
-    ReglasIndice,
-    ReglasDesc,
-  } from "../../components/index";
-  
-  const RenderReglas = () => {
-    return (
-      <>
-        <div className="reglasContainer">
-          <ReglasIndice />
-          <ReglasDesc />
-        </div>
-      </>
-    );
-  };
-  
-  export { RenderReglas };
+import { ReglasIndice, ReglasDesc } from "../../components/index";
+import "./style.css";
+
+const RenderReglas = () => {
+  return (
+    <>
+      <div className="reglasContainer">
+        <ReglasIndice />
+        <ReglasDesc />
+      </div>
+    </>
+  );
+};
+
+export { RenderReglas };
